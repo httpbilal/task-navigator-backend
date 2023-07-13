bb88253c639d49373aca2fbd6f77457ffef8a4fc
FalseGenius <billionairejackal@gmail.com>
2023-07-14 00:13:00 +0500

    readme.md file updated

--- routes/RedisRoutes/RedisRoutes.php
+++ routes/RedisRoutes/RedisRoutes.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 // Public routes
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('fetch-top-tasks/{id}', [RedisTaskFetchController::class, 'fetchTopPriorityTasks']);
-    Route::get('saveToRedis/{id}', [RedisTaskSaveController::class, 'savePriorityTasks']);
+    Route::get('fetch-top-tasks/{userId}', [RedisTaskFetchController::class, 'fetchTopPriorityTasks']);
+    Route::post('saveToRedis/{userId}', [RedisTaskSaveController::class, 'savePriorityTasks']);
 
 });
