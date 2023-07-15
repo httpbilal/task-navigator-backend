beefb9320fbd8f352b29801d9289503dcdb03c98
httpbilal <malikbilaljavaid@gmail.com>
2023-07-15 19:47:38 +0500

    Workspaces CRUD is fully functional

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -5,13 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UsersTasks extends Model
+class Workspace extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'task_id'];
+    protected $fillable = ['name', 'owner'];
 
     protected $hidden = [
         'created_at',
         'updated_at'
     ];
+
+    public function ownerUser()
+    {
+        return $this->belongsTo(User::class, 'owner');
+    }
 }
