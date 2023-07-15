a156e6f2e17d52a9b8af196753e7709f3a8fadc1
httpbilal <malikbilaljavaid@gmail.com>
2023-07-15 20:33:11 +0500

    Project Model, Controller and routes are integrated

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'workspace'];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class, 'workspace');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ require_once __DIR__ . '/TaskRoutes/TaskRoutes.php';
 require_once __DIR__ . '/RedisRoutes/RedisRoutes.php';
 require_once __DIR__ . '/TaskUserRoutes/TaskUserRoutes.php';
 require_once __DIR__ . '/WorkspaceRoutes/WorkspaceRoutes.php';
+require_once __DIR__ . '/ProjectsRoutes/ProjectsRoutes.php';
