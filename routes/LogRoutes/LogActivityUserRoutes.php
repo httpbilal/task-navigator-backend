228a3d814aa3fbf53a384bf7f7ccb2a555b4b772
Ali <gujjaarr776@gmail.com>
2023-07-16 21:55:54 +0500

    Log Activity Complete

--- routes/LogRoutes/LogActivityUserRoutes.php
+++ routes/LogRoutes/LogActivityUserRoutes.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 // Protected routes
 Route::group(['middleware' => ['LogUserActivity', 'auth:api']], function () {
     Route::get('user_logs', [Log_activityController::class, 'index']);
+    Route::get('user_logs/nonauth', [Log_activityController::class, 'record']);
+    Route::get('user_logs/auth', [Log_activityController::class, 'showAuthenticated']);
     Route::get('user_log/{email}', [Log_activityController::class, 'show']);
     Route::delete('destory_log/{email}', [Log_activityController::class, 'destroy']);
 });
