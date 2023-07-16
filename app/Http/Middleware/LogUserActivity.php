2c82b245b35adb40971f67c6e25a02534ab69198
Ali <gujjaarr776@gmail.com>
2023-07-16 19:28:23 +0500

    Modified Log Activity

--- app/Http/Middleware/LogUserActivity.php
+++ app/Http/Middleware/LogUserActivity.php
@@ -5,18 +5,31 @@ namespace App\Http\Middleware;
 use App\Models\ActivityLog;
 use Closure;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 class LogUserActivity
 {
     public function handle(Request $request, Closure $next)
     {
-        // Log the user activity
-        $activityLog = new ActivityLog();
+        // Check if the user is authenticated
+        if (Auth::check()) {
+            // Get the authenticated user
+            $user = Auth::user();
+
+            // Log the user activity
+            $activityLog = new ActivityLog();
+            $activityLog->email = $user->email;
+            $activityLog->token = $user->api_token;
+        } else {
+            // Log the activity for unauthenticated users
+            $activityLog = new ActivityLog();
+            $activityLog->email = null;
+            $activityLog->token = null; 
+        }
+
+        // Log the common details
         $activityLog->url = $request->fullUrl();
         $activityLog->method = $request->method();
-        $activityLog->email = $request->email;
-        // $activityLog->payload = $request->all();
         $activityLog->ip = $request->ip();
         $activityLog->user_agent = $request->header('User-Agent');
         $activityLog->save();
--- app/Models/ActivityLog.php
+++ app/Models/ActivityLog.php
@@ -9,6 +9,6 @@ class ActivityLog extends Eloquent
     protected $connection = 'mongodb';
     protected $collection = 'activity_logs';
     protected $fillable = [
-        'url', 'method', 'payload', 'ip', 'user_agent', 'payload',  'response'
+        'url', 'method', 'payload', 'ip', 'token' , 'user_agent', 'response'
     ];
 }
